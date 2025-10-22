3a13bd724b38a5f2c8bbb40c0136e52b2d99fe83
Victor Barreto <victorbarretolins@gmail.com>
2025-10-22 11:49:33 -0300

    feat: refactor build configuration to use version catalog and add initial test classes

--- integrationTest/src/main/java/config/BaseIntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.testcontainers.utility.DockerImageName;
-
-public class BaseIntegrationTest {
-
-    DockerImageName POSTGRES_IMAGE = DockerImageName.parse("postgres:15");
-
-}
---
+++ integrationTest/src/test/java/TestingTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class TestingTest {
+
+    @Test
+    void contextLoads() {
+        // integration test placeholder
+    }
+
+}
